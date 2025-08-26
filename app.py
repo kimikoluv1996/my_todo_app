4675ae035a15c617d4ffe28cc4490a8a5258f47d
kimikoluv1996 <kimikoluv1996@proton.me>
2025-08-26 17:16:14 -0400

    app finished

--- app.py
+++ app.py
@@ -1,14 +1,29 @@
 from flask import Flask
+from flask import request, render_template, redirect, url_for
 
 item = dict(task="", completed=False)
-items = list()
+tasks = list()
+task_id_counter = 0
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    global task_id_counter
+    if request.method == 'POST':
+        form_data = request.form['task']
+        tasks.append({'id': task_id_counter, 'task': form_data, 'completed': False})
+        task_id_counter += 1
+        return redirect(url_for('index'))
+    return render_template('index.html', tasks=tasks)
 
-def return_html():
-    return "Hello World"
+@app.route('/complete/<int:task_id>')
+def complete(task_id):
+    for task in tasks:
+        if task['id'] == task_id:
+            task['completed'] = not task['completed']
+            break
+    return redirect(url_for('index'))
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
